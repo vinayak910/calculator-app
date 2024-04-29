738f3c5db93b4e24c4c654b590691bf634aa4d91
Vinayak Chhabra <vinayakchhabra545.vc@gmail.com>
2024-04-29 06:43:04 +0530

    multiply

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from calculate_func import do_addition , do_substraction
+from multiply import do_multiplication
 def main():
     print("Welcome to the calculator app")
     print("""Select the function from the given options:
-             1. ADD
-             2. Substract
-          """)
+1. ADD
+2. Substract
+3. Multiply
+        """)
     
-    user_input = input("Select the function")
+    user_input = input("Select the function;")
     a = int(input("Enter num1:"))
     b = int(input("Enter num2:"))
 
@@ -14,6 +16,8 @@ def main():
         result = do_addition(a , b)
     elif user_input == '2':
         result = do_substraction(a , b)
+    elif user_input == '3':
+        result = do_multiplication(a , b)
     else:
         print("Invalid Choice")
 
